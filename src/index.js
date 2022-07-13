a620176dc342a824915ef928c0db34d12c23a5d0
Evans Rutto <robbyevans001@gmail.com>
2022-07-13 13:23:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import MesaVerde from "./parks/MesaVerde"
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+ReactDOM.render(<ColoradoStateParks />
+, document.getElementById("root"));
